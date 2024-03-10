ea1e8fbb2e816a952aabd32a4d32bbe234e05466
Dev-Olena <alenagruda@gmail.com>
2024-03-10 12:08:29 +0200

    signUp user

--- server/controllers/User.controller.js
+++ server/controllers/User.controller.js
@@ -2,14 +2,23 @@ const {User} = require('../models');
 const {deletePassword} = require('../utils/deletePassword');
 
 
-module.exports.createUser = async (req, res, next) => {
+module.exports.signUp = async (req, res, next) => {
     try {
         const {body} = req;
         const createdUser = await User.create(body); 
         const readyUser = deletePassword(createdUser)
         // перед тим, як повертати юзера, треба з об'єкта видалити пароль
+        //TODO: створити сесію юзера
         res.status(201).send({data: readyUser});
     } catch(error) {
          next(error)
     }
 };
+
+module.exports.signIn= async (req, res, next) => {
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+}
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -2,7 +2,8 @@ const userRouter = require('express').Router();
 const UserController = require('../controllers/User.controller');
 const {hashPass} = require('../middlewares/hashPass');
 
-userRouter.post('/', hashPass, UserController.createUser);
+userRouter.post('/sign-up', hashPass, UserController.signUp);
+userRouter.post('/sign-in', )
 
 module.exports = userRouter;
 
