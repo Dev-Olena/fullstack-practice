82b02696255169ec5b34457a361ed6da026b35ec
Dev-Olena <alenagruda@gmail.com>
2024-03-14 19:32:34 +0200

    BrouserRouter Home/Dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
 import React, {useState} from "react";
+
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   const [user, setUser] = useState(null);
   return (
-    <Home setUser={setUser}/>
+    <BrowserRouter>
+    <Switch>
+      <Route path='/' exact >
+        <Home setUser={setUser} />
+      </Route>
+      <Route path='/messenger'>
+        <Dashboard/>
+      </Route>
+    </Switch>
+     </BrowserRouter>
   );
 }
 
