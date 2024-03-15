d90f940986fe468d770842a6e1fa34a0f228ed09
Dev-Olena <alenagruda@gmail.com>
2024-03-15 09:58:48 +0200

    Add intercetors.request

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -4,5 +4,27 @@ const httpClient = axios.create({
     baseURL: 'http://localhost:5000/api'
 })
 
+httpClient.interceptors.request.use((config) => {
+    const token = localStorage.getItem('accessToken');
+    if (token) {
+        // маємо додати до запиту заголовок Authorization з цим токеном
+        config.headers = {
+            ...config.headers,
+            Authorization: `Bearer ${token}`
+        }
+    }
+    return config;
+}, 
+(err) => { Promise.reject(err)});
+
+
+// Auth API
+
 export const signIn = async (data) => await httpClient.post('/users/sign-in', data);
 export const signUp = async (data) => await httpClient.post('/users/sign-up', data);
+
+
+
+// Chat API
+
+export const getUserChats = async () => await httpClient.get('/chats')
