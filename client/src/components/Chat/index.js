f9849b3ff26f083b936e65f6928c691654037bf2
Dev-Olena <alenagruda@gmail.com>
2024-03-25 13:22:04 +0200

    change Chat(redux)

--- client/src/components/Chat/index.js
+++ client/src/components/Chat/index.js
@@ -1,34 +1,39 @@
 import React, {useContext, useEffect, useState, useRef} from 'react';
+import {connect} from 'react-redux';
 import styles from './Chat.module.css';
-import ChatContext from '../../contexts/ChatContext';
+// import ChatContext from '../../contexts/ChatContext';
 import {getOneChat} from '../../api/index';
 import ChatItem from './ChatItem/index';
 
 const Chat = (props) => {
-    const [currentChat] = useContext(ChatContext);
-    const [chatStory, setChatStory] = useState([]);
+    // const [currentChat] = useContext(ChatContext);
+    // const [chatStory, setChatStory] = useState([]);
     const scrollRef = useRef(null);
 
 
-    useEffect (() => {
-        if (currentChat) {
-            getOneChat(currentChat._id)
-            .then(res => {
-                setChatStory(res.data.data.messages);
+    // useEffect (() => {
+    //     if (currentChat) {
+    //         getOneChat(currentChat._id)
+    //         .then(res => {
+    //             setChatStory(res.data.data.messages);
 
-            })
-        }
-    }, [currentChat]);
+    //         })
+    //     }
+    // }, [currentChat]);
 
     useEffect(() => {
         scrollRef.current.scrollIntoView();
     })
     return (
         <section className={styles.chat}>
-            {chatStory && chatStory.map(message => <ChatItem data={message} key={message._id}/>)}
+            {/* {chatStory && chatStory.map(message => <ChatItem data={message} key={message._id}/>)} */}
+            {props.currentChat.messages.map(message => <ChatItem data={message} key={message._id}/>)}
             <div ref={scrollRef}></div>
         </section>
     );
 }
 
-export default Chat;
+// export default Chat;
+const mapStateToProps = ({currentChat}) => ({currentChat})
+
+export default connect(mapStateToProps)(Chat);
