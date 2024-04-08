bd573f8eaee2b3b33545ea436755be3561a0c58c
Dev-Olena <alenagruda@gmail.com>
2024-04-08 13:58:37 +0300

    drag and drop

--- client/src/components/MessageArea/index.js
+++ client/src/components/MessageArea/index.js
@@ -2,10 +2,12 @@ import React, {useState} from 'react';
 import {connect} from 'react-redux';
 import styles from './Message.module.css';
 import {addNewMessageRequest} from '../../actions/actionCreators';
+import cx from 'classnames';
 
 const MessageArea = (props) => {
     const [text, setText] = useState('');
     const [image, setImage] = useState();
+    const [drag, setDrag] = useState(false);
 
     const submitHandler = (e) => {
         e.preventDefault();
@@ -33,8 +35,38 @@ const MessageArea = (props) => {
     const imageHandler = (event) => {
         setImage(event.target.files[0]);
     }
-    return (
-        <form className={styles['message-box']} onSubmit={submitHandler}>
+
+    const dragEnter = (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        setDrag(true)
+    };
+    const dragOver = (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+    };
+
+    const dragLeave = (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        setDrag(false);
+    };
+
+    const drop = (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        console.log('drop');
+        setImage(event.dataTransfer.files[0]);
+        setDrag(false);
+    };
+
+    const cnames = cx(styles['message-box'], {
+        [styles['drag-active']]: drag
+    })
+    const viewDrag = () => 'Drop picture to send it'
+
+    const viewForm = () => (
+        <>
             <textarea className={styles.text} value={text} onChange={changeHandler}/>
             <article className={styles['buttons-wraper']}>
                 <button type="submit" className={styles.button}><img src='assets/icons/avion.png' className={styles.icon}/></button>
@@ -43,7 +75,19 @@ const MessageArea = (props) => {
                     <img className={styles['input-file-icon']} src='assets/icons/clip.png' />
                 </div>
             </article>
-
+        </>
+    )
+    return (
+        <form 
+        // className={styles['message-box']} 
+        onSubmit={submitHandler}
+        onDragEnter={dragEnter}
+        onDragOver={dragOver}
+        onDragLeave={dragLeave}
+        onDrop={drop}
+        className={cnames}>
+            
+        {drag ? viewDrag()  : viewForm()}
             
             
         </form>
@@ -55,3 +99,11 @@ const mapDispatchToProps = {
     addNewMessageRequest
 }
 export default connect (mapStateToProps, mapDispatchToProps)(MessageArea);
+
+/*
+TODO-in-future: доопрацювати компоненту, створивши мініатюру файлу для перед-перегляду до відправки.
+Варіанти реалізації:
+1. Ще один стейт, в якому буде зберігатись зчитаний файл, що встановлюється в якості src для картинки
+2. ПЕреробити drag-n-drop флоу на окрему компоненту, яка працюватиме за паттерном render-props
+*/
+
